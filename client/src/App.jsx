089d75f4ad16b2355776ed3dc23d1f3c1e73e5f8
alasdairwood04 <alasdair.wood9@btinternet.com>
2025-09-17 12:23:55 +0100

    implemented directory structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold bg-amber-400 p-4 px-12 text-center">Intramural Sport App</h1>
+    </>
+  )
+}
+
+export default App
